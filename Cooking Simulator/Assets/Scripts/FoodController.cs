5644b39a3d7f4c8ac3ecd8a3f6b9f0a8c518fafc
iSantiWinky <64086885+iSantiWinky@users.noreply.github.com>
2020-06-02 01:32:51 -0500

    Intento del sistema de comida y de instanciar personitas

---
+++ Cooking Simulator/Assets/Scripts/FoodController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodController : MonoBehaviour
+{
+    public Transform breadTop;
+    public Transform breadDown;
+    public Transform meat;
+
+    void Start()
+    {    
+    }
+
+    void Update()
+    {       
+    }
+}
